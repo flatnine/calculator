69cca9620ee7e31cf740a61b232ccd3c0697d0c2
Jon Burman <jonathanburman@timeout.com>
2014-02-10 17:25:29 +0000

    Added phpunit and test code

---
+++ app/libraries/Calculator.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Calculator  {
+
+	public function add($x, $y)
+	{
+		return $x + $y;
+	} 
+
+}
+
---
+++ test/CalculatorTest.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+class CalculatorTest extends PHPUnit_Framework_TestCase {
+	public function testAddNumbers()
+	{
+		$calc = new Calculator;
+		$this->assertEquals(4 , $calc->add(2,2));
+	}
+}
